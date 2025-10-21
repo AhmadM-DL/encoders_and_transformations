f2d7f1db6a243fc1d55bf48c1ba644bac3256f03
Ahmad Mustapha <ahmad.m.mustapha@hotmail.com>
2025-10-21 11:58:36 +0300

    Hot Fix

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import subprocess
-import os, sys
+import os, re
+from tqdm import tqdm
 
 def download_using_axel(url, output_dir, output_filename, num_connections = 10):
     
@@ -13,13 +14,27 @@ def download_using_axel(url, output_dir, output_filename, num_connections = 10):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    cmd = ["axel",
+    print(f"Downloading {url} using Axel with {num_connections} connections...")
+
+    process = subprocess.Popen(
+        ["axel",
             "-n", str(num_connections),
             "-o", os.path.join(output_dir, output_filename),
-            url]
-    
-    print(f"Downloading {url} using Axel with {num_connections} connections...")
-    
-    result = subprocess.run(cmd, check=True)
-    
+            url],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
+
+    pbar = tqdm(total=100, desc="Downloading", unit="%")
+
+    for line in process.stdout:
+        match = re.search(r'(\d{1,3})%', line)
+        if match:
+            progress = int(match.group(1))
+            pbar.n = progress
+            pbar.refresh()
+
+    process.wait()
+    pbar.close()
     print("Download completed successfully!")
