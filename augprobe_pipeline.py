ad7a2fe5cb04d128eed255be9c4c97603e85993f
Ahmad Mustapha <ahmad.m.mustapha@hotmail.com>
2025-10-27 22:36:24 +0200

    Hot Fix

--- augprobe_pipeline.py
+++ augprobe_pipeline.py
@@ -3,21 +3,24 @@ import random, os, json
 from metrics import top_k_augmentations_recall, augmentations_rank
 from encoders import get_features
 
-def probe(encoder, dataset, transformation, n_augmentations=10, sample_size=500, encoder_target_dim=768, random_state=42, chkpt_path="./chkpt"):
+def probe(encoder, dataset, transformation, n_augmentations=10, sample_size=500, encoder_target_dim=768, random_state=42, chkpt_path="./chkpt", verbose=True):
     
     # Set random seed
     random.seed(random_state)
     np.random.seed(random_state)
 
     # Create checkpoint
+    if verbose: print("Checking path ...")
     if not os.path.exists(chkpt_path):
         os.mkdir(chkpt_path)
     
     # Take a random subset
+    if verbose: print(f"Sampling {sample_size} images ...")
     sample_indices = random.sample(range(len(dataset)), min(sample_size, len(dataset)))
     sample_data = [dataset[i] for i in sample_indices]
     
     # Apply transformations on each image in the sample
+    if verbose: print("Applying transformations ...")
     all_images = []
     image_ids = []
     
@@ -25,14 +28,13 @@ def probe(encoder, dataset, transformation, n_augmentations=10, sample_size=500,
         # Original image
         all_images.append(image)
         image_ids.append(idx)
-        
         # Generate augmentations
-        for aug_idx in range(n_augmentations):
-            augmented_image = transformation(image)
-            all_images.append(augmented_image)
-            image_ids.append(idx)  # Same ID for original and its augmentations
-    
+        augmented_images = transformation([image]*n_augmentations)
+        all_images.extend(augmented_images)
+        image_ids.extend([idx]*n_augmentations)  # Same ID for original and its augmentations
+
     # Get the features of each image and augmentations
+    if verbose: print("Getting images embeddings ...")
     features = []
     for image in all_images:
         feature = get_features(encoder, image, encoder_target_dim, "cuda")
@@ -40,10 +42,12 @@ def probe(encoder, dataset, transformation, n_augmentations=10, sample_size=500,
     features = np.vstack(features).astype('float32')
     
     # Compute metrics
+    if verbose: print({"Computing metrics ..."})
     top_k_aug_recall_scores = top_k_augmentations_recall(features, image_ids, n_augmentations, n_augmentations)
     aug_avg_rank_scores, aug_min_rank_scores, aug_max_rank_scores = augmentations_rank(features, image_ids)
     
     # Store the metrics in checkpoint format
+    if verbose: print("Saving to chekpoint ...")
     encoder_name = getattr(encoder, '__name__', str(encoder))
     dataset_name = getattr(dataset, 'dataset_name', 'unknown_dataset')
     transformation_name = getattr(transformation, '__name__', str(transformation))
